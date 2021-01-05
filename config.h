69f75621a64758d0fbebe50f144686e29d90a476
Wilkuu-1 <jstachurski9991@gmail.com>
2021-01-05 21:50:15 +0100

    fixed semicolon

--- config.h
+++ config.h
@@ -57,9 +57,9 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_main, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "urxvt", NULL };
-static const char *flameshotcmd[] = {"flameshot","gui",NULL}
+static const char *flameshotcmd[] = {"flameshot","gui",NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
